51cbf1fe52da9d6b432d86d93be8ee62928670c7
Fahim Farook <fahimf@gmail.com>
2013-07-15 13:14:17 +0800

    Initial Commit

---
+++ PeopleDatabase/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PeopleDatabase
+//
+//  Created by Fahim Farook on 15/7/13.
+//  Copyright (c) 2013 Fahim Farook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
